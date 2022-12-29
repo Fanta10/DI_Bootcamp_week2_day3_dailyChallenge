f5e527fce711dfafdb97e3efb59de8b80e994804
Fanta10 <fantakamate107@gmail.com>
2022-12-29 23:51:30 +0000

    commit 1

---
+++ script.js
@@ -0,0 +1,22 @@
+let etoile = " ";
+
+for ( let i = 1; i < 6;  i++){
+    etoile = etoile  +  "*" ;
+    console.log(etoile);
+    
+}
+
+
+
+console.log("boucles imbriquees ci-dessus: ");
+let etoiles = " ";
+for ( let j = 1; j < 6;  j++){
+    for(let k = 1; k< 6; k++){
+        etoiles = etoiles  +  "*" ;
+    
+
+    }
+    console.log(etoiles);
+    
+}
+
